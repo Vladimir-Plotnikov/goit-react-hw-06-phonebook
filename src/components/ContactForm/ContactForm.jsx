1228ba8cd95f1adb08fb084b843fae4c16173c55
Volodymyr <vovdata@gmail.com>
2023-03-03 20:37:44 +0100

    Formik123
    Formik321

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,15 +1,16 @@
 import { nanoid } from 'nanoid'
+import { Formik, Form, Field } from 'formik';
 import { useDispatch } from 'react-redux'
 import { addContact } from 'redux/phonebookSlice';
-import { Formik } from 'formik';
 
 export function ContactForm() {
+
     const dispatch = useDispatch();
     const initialValues = {
         name: '',
         number: '',
     };
-    const handleSubmit = (values,{resetForm}) => {
+    const handleSubmit = (values, { resetForm }) => {
         
         const newContact = {
             id: nanoid(),
@@ -23,43 +24,40 @@ export function ContactForm() {
     return (
         <Formik
             initialValues={initialValues}
-            onSubmit={handleSubmit}
+            onSubmit = {handleSubmit}
         >
-        <div className='ContactForm'>
-       <form
-          className='inputButton'
-          autoComplete='off'
-            >
-
-            
+<div className='ContactForm'>
+       <Form
+          className='inputButton'>
     <h2>Name</h2>
         <label>
-        <input className='InputField' 
-            id='name'
+        <Field className='InputField' 
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            placeholder=' '                    
+            required
+                                
         /></label> 
         
     <h2>Number</h2>
         <label>
-        <input
-            id='number'
+        <Field
             type="tel"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            placeholder=''
+            required
+            
         /></label>   
+             
         <button 
             className='Button'
             type='submit'>Add contact
         </button>
-                </form>
-                </div>
+        </Form>
 
+            </div>
     </Formik>
     )
 };
